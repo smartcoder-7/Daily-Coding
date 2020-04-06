e7268fb89b6edbf50d77cea7d0b4e0d32e8f2dd7
Smart Coder <smartcoder415@gmail.com>
2020-04-05 23:08:20 -0400

    added solution with has

--- most-frequent-value-in-array.js
+++ most-frequent-value-in-array.js
@@ -4,18 +4,18 @@
 function findMostFrequentElement(A) {
   if (!A.length) return null;
 
-  A.sort((a, b) => a - b);
+  let sorted = A.concat().sort((a, b) => a - b);
 
   let tempMax = -1;
   let value = null;
   let count = 1;
   for (let i = 1; i < A.length; i += 1) {
-    if (A[i] === A[i - 1]) {
+    if (sorted[i] === sorted[i - 1]) {
       count += 1;
     } else {
       if (count > tempMax) {
         tempMax = count;
-        value = A[i - 1];
+        value = sorted[i - 1];
       }
       count = 1;
     }
@@ -24,25 +24,26 @@ function findMostFrequentElement(A) {
   return value;
 }
 
-console.log(
-  findMostFrequentElement([
-    1,
-    5,
-    1,
-    2,
-    3,
-    5,
-    5,
-    5,
-    2,
-    1,
-    2,
-    0,
-    5,
-    2,
-    6,
-    7,
-    5,
-    2,
-  ]),
-);
+function solutionWithHash(A) {
+  if (!A.length) return null;
+
+  let hash = {};
+  A.forEach(current => (hash[current] = hash[current] ? ++hash[current] : 1));
+
+  let tempMax = -1;
+  let value = null;
+
+  Object.entries(hash).forEach((current, index) => {
+    if (current[1] > tempMax) {
+      tempMax = current[1];
+      value = current[0];
+    }
+  });
+
+  return value;
+}
+
+const input = [1, 5, 1, 2, 3, 5, 5, 5, 2, 1, 2, 0, 5, 2, 6, 7, 5, 2];
+console.log(findMostFrequentElement(input));
+
+console.log(solutionWithHash(input));
