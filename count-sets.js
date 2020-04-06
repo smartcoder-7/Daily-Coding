8a483c3c22a334999afa3820b91b7768ae3a01ca
Smart Coder <smartcoder415@gmail.com>
2020-04-06 08:16:44 -0400

    added dp solution

--- count-sets.js
+++ count-sets.js
@@ -14,5 +14,33 @@ function rec(input, total, i) {
   return rec(input, total - input[i], i - 1) + rec(input, total, i - 1);
 }
 
-const input = [2, 4, 6, 10];
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+console.time('recursive');
 console.log(countSet(input, 6));
+console.timeEnd('recursive');
+
+//time complexity <= total*n*2 + 1 == O(n)
+function countSetWithDP(input, total) {
+  const memo = {};
+  return dp(input, total, input.length - 1, memo);
+}
+
+function dp(input, total, i, memo) {
+  const key = total + '' + i;
+  let result;
+  if (memo[key]) return memo[key];
+
+  if (total === 0) return 1;
+  if (total < 0) return 0;
+  if (i < 0) return 0;
+  if (total < input[i]) result = rec(input, total, i - 1, memo);
+  result =
+    rec(input, total - input[i], i - 1, memo) + rec(input, total, i - 1, memo);
+
+  memo[key] = result;
+  return result;
+}
+
+console.time('with-dp');
+console.log(countSetWithDP(input, 6));
+console.timeEnd('with-dp');
