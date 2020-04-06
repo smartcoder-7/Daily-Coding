9c27604fe3d0917bacaaa4bfb777588196e4b11d
Smart Coder <smartcoder415@gmail.com>
2020-04-06 07:37:26 -0400

    solved encoding

--- encoding-num-ways.js
+++ encoding-num-ways.js
@@ -7,7 +7,7 @@ function helper(data, k) {
   let s = data.length - k;
   if (data[s] === '0') return 0; // if starting point is zero, return zero
 
-  result = helper(data, k - 1);
+  let result = helper(data, k - 1);
   if (k >= 2 && Number(data.slice(s, s + 2))) {
     result += helper(data, k - 2);
   }
@@ -17,6 +17,27 @@ function helper(data, k) {
 
 function solution(input) {
   return helper(input, input.length);
+}
+
+function helperWithDP(data, k, memo) {
+  if (k === 0) return 1;
+  let s = data.length - k;
+
+  if (data[s] === '0') return 0; // if starting point is zero, return zero
+  if (memo[k] !== null) return memo[k];
+
+  let result = helperWithDP(data, k - 1, memo);
+  if (k >= 2 && Number(data.slice(s, s + 2))) {
+    result += helperWithDP(data, k - 2, memo);
+  }
+
+  memo[k] = result;
+  return result;
+}
+
+function solutionWithDP(input) {
+  const memo = Array(input.length + 1).fill(null);
+  helperWithDP(data, k, memo);
 }
 
 console.log(solution('12'));
