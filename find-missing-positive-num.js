3acb17faad022854be540467c7016a05fd55436a
Smart Coder <smartcoder415@gmail.com>
2020-04-08 23:03:01 -0400

    solved problem

--- find-missing-positive-num.js
+++ find-missing-positive-num.js
@@ -4,3 +4,31 @@
 //In other words, find the lowest positive integer that does not exist in the array.
 //The array can contain duplicates and negative numbers as well.
 // For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+function findMissingPositive(input) {
+  let min = 1;
+  let possibleValues = new Set();
+
+  for (let i = 0; i < input.length; i += 1) {
+    if (input[i] > min) {
+      possibleValues.add(input[i]);
+    } else if (min === input[i]) {
+      min += 1;
+
+      while (possibleValues.has(min)) {
+        possibleValues.delete(min);
+
+        min += 1;
+      }
+    }
+  }
+
+  return min;
+}
+
+const input1 = [2, 3, -7, 6, 8, 1, -10, 15];
+const input2 = [1, 1, 0, -1, -2];
+
+console.time('set method used');
+console.log(findMissingPositive(input1));
+console.timeEnd('set method used');
