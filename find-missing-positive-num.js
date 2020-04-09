59c404b6f3491c6f217a82eb9523735a00793291
Smart Coder <smartcoder415@gmail.com>
2020-04-09 06:01:32 -0400

    added hash solution

--- find-missing-positive-num.js
+++ find-missing-positive-num.js
@@ -26,9 +26,31 @@ function findMissingPositive(input) {
   return min;
 }
 
+function solutionWithObject(input) {
+  const possibleValues = input.reduce((acc, current, index) => {
+    if (current > 0) acc[current] = acc[current] + 1 || 1;
+    return acc;
+  }, {});
+
+  let index = 1;
+  while (1) {
+    if (!possibleValues[index]) {
+      return index;
+    }
+
+    index += 1;
+  }
+}
+
 const input1 = [2, 3, -7, 6, 8, 1, -10, 15];
-const input2 = [1, 1, 0, -1, -2];
+const input2 = [1, 1, 1, 0, -1, -2];
 
 console.time('set method used');
 console.log(findMissingPositive(input1));
+console.log(findMissingPositive(input2));
 console.timeEnd('set method used');
+
+console.time('hash method used');
+console.log(solutionWithObject(input1));
+console.log(solutionWithObject(input2));
+console.timeEnd('hash method used');
