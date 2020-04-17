9a9da13b8f40e3bca2a91e16acf1aad2f525ee7e
Smart coder <smartcoder415@gmail.com>
2020-04-17 11:38:01 -0400

    added longest-substring

--- staircase.js
+++ staircase.js
@@ -8,4 +8,5 @@
 // 1, 2, 1
 // 1, 1, 2
 // 2, 2
-// What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+// What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X?
+// For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
